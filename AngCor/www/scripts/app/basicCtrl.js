e5ce4b809c0cbafafa38590d44071f83e42eedc2
Ewald Ramp <ewald.ramp@sstid.com>
2016-06-29 16:04:08 -0400

    Basic Cordova Project utilizing AngularJS

---
+++ AngCor/www/scripts/app/basicCtrl.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+    angular.module("angCor.controllers").controller('basicCtrl', ['$timeout', basicCtrl]);
+
+
+    function basicCtrl($timeout) {
+        var that = this;
+        that.log = [];
+        that.beacons = {};
+        that.beaconsRead = 0;
+
+        var i = 0;
+        var Log = function (text) {
+            console.log(text);
+            i++;
+            that.log.push(i + "-" + text);
+        }
+
+        document.addEventListener("deviceready", function () {
+            $timeout(function () {
+                Log(new Date());
+                Log("Device Ready");
+            });
+
+            document.addEventListener('pause', function () { onPause(); }, false);
+            document.addEventListener('resume', function () { onResume(); }, false);
+        }, false);
+
+        function onPause() {
+            Log('Pausing');
+        }
+
+        function onResume() {
+            Log('Resuming');
+        }
+
+    }
+
+})();
--- AngCor/www/scripts/index.js
+++ AngCor/www/scripts/index.js
@@ -4,27 +4,6 @@
 // and then run "window.location.reload()" in the JavaScript Console.
 (function () {
     "use strict";
-
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
-
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-    };
-
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    angular.module('angCor.controllers', []);
+    angular.module('angCor', ['ngCordova', 'angCor.controllers']);
+})();
