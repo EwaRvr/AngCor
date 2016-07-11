c1dfa95e2254b6b4bc51559939fed56eed3ba87e
Ewald Ramp <ewald.ramp@sstid.com>
2016-07-11 15:04:52 -0400

    Added Dialog feedback to user if battery level is low - Added cordova-plugin-dialogs plugin

--- AngCor/www/scripts/app/basicCtrl.js
+++ AngCor/www/scripts/app/basicCtrl.js
@@ -1,9 +1,9 @@
 ﻿(function () {
     'use strict';
-    angular.module("angCor.controllers").controller('basicCtrl', ['$timeout','$rootScope','$cordovaBatteryStatus', basicCtrl]);
+    angular.module("angCor.controllers").controller('basicCtrl', ['$timeout', '$rootScope', '$cordovaBatteryStatus', '$cordovaDialogs', basicCtrl]);
 
 
-    function basicCtrl($timeout, $rootScope, $cordovaBatteryStatus) {
+    function basicCtrl($timeout, $rootScope, $cordovaBatteryStatus,$cordovaDialogs) {
         var that = this;
         that.log = [];
         that.beacons = {};
@@ -12,14 +12,14 @@
         that.isPluggedIn = false;
 
         var i = 0;
-        var Log = function (text) {
+        var Log = function(text) {
             console.log(text);
             i++;
             that.log.push(i + "-" + text);
         }
 
-        document.addEventListener("deviceready", function () {
-            $timeout(function () {
+        document.addEventListener("deviceready", function() {
+            $timeout(function() {
                 Log(new Date());
                 Log("Device Ready");
             });
@@ -27,15 +27,15 @@
             document.addEventListener('pause', onPause, false);
             document.addEventListener('resume', onResume, false);
 
-$rootScope.$on('$cordovaBatteryStatus:status', function(scope,status) {
-        onBatteryStatusUpdate(status, '');
-});
-$rootScope.$on('$cordovaBatteryStatus:low', function (scope, status) {
-    onBatteryStatusUpdate(status, 'Low ');
-});
-$rootScope.$on('$cordovaBatteryStatus:critical', function (scope, status) {
-    onBatteryStatusUpdate(status, 'Critical ');
-});
+            $rootScope.$on('$cordovaBatteryStatus:status', function(scope, status) {
+                onBatteryStatusUpdate(status, '');
+            });
+            $rootScope.$on('$cordovaBatteryStatus:low', function(scope, status) {
+                onBatteryStatusLow(status);
+            });
+            $rootScope.$on('$cordovaBatteryStatus:critical', function(scope, status) {
+                onBatteryStatusCritical(status);
+            });
 
         }, false);
 
@@ -48,9 +48,21 @@ $rootScope.$on('$cordovaBatteryStatus:critical', function (scope, status) {
         }
 
         function onBatteryStatusUpdate(result, state) {
-            Log('Battery Level ' + state + ":" + result.level + (result.isPlugged?'% ⚡':'%'));
-            that.batteryLevel = result.level;       // (0 - 100)
-            that.isPluggedIn = result.isPlugged;   // bool
+            Log('Battery Level ' + state + ":" + result.level + (result.isPlugged ? '% ⚡' : '%'));
+            that.batteryLevel = result.level; // (0 - 100)
+            that.isPluggedIn = result.isPlugged; // bool
+        }
+
+        function onBatteryStatusLow(status) {
+            $cordovaDialogs.beep(1);
+            $cordovaDialogs.alert('Your battery level is Low.', 'WARNING!', "OK");
+            onBatteryStatusUpdate(status, 'Low ');
+        }
+
+        function onBatteryStatusCritical(status) {
+            $cordovaDialogs.beep(4);
+            $cordovaDialogs.alert('Your battery level is at critical levels. Plug in NOW!', 'WARNING!', 'I Know what I am doing!');
+            onBatteryStatusUpdate(status, 'Critical ');
         }
 
     }
